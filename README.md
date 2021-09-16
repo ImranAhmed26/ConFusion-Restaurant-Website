74bdba6e3c4a355ed16c55db4e2fa4154f8cd6b7
Imran <imran.ahmed26@outlook.com>
2021-09-17 03:15:02 +0600

    added all files including npm pckgs

---
+++ README.md
@@ -0,0 +1,1 @@
+# Coursera-Bootstrap-Part
---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css', ['sass']);
+
+};
